e2d8b29a5d53fb5c39159c90764b30295c93cebf
Nursat <makhsotnursat@gmail.com>
2023-02-20 14:56:27 +0500

    Первая задача

---
+++ simple_digits/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+//Вывести на экран все простые числа из промежутка от 0 до N.
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"N = {n}");
+
+for (int i = 2; i <= n; i++)
+{
+	if (IsSimpleDigit(i))
+	{
+		Console.Write(i + " ");
+	}
+}
+
+bool IsSimpleDigit(int digit)
+{
+	if (digit == 2 
+		|| digit == 3
+		|| digit == 5
+		|| digit == 7)
+	{
+		return true;
+	}
+
+	if (digit % 2 == 0 
+		|| digit % 3 == 0 
+		|| digit % 5 == 0
+		|| digit % 7 == 0)
+	{
+		return false;
+	}
+	return true;
+}
