64e9a24efcc61af8f136ddbd4bd668888103f119
Nursat <makhsotnursat@gmail.com>
2023-02-20 15:37:29 +0500

    Задачи: find_max, palindrome

---
+++ max_digit/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+var size = Convert.ToInt32(Console.ReadLine());
+
+
+var nums = new int[size];
+
+for (int i = 0; i < nums.Length; i++)
+{
+	nums[i] = i;
+}
+
+
---
+++ palindrome/Program.cs
@@ -0,0 +1,39 @@
+﻿
+// Задача — проверить, является ли это число палиндромом.
+
+var x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(IsPalindrome(x));
+
+
+bool IsPalindrome(int digit)
+{
+	if (digit == 1)
+	{
+		return true;
+	}
+	else
+	{
+		digit = digit ^ ReverseDigit(digit);
+		if (digit == 0)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+int ReverseDigit(int Digit)
+{
+	var result = "";
+	var digit = Digit.ToString().Length;
+
+	for (int i = 0; i < digit; i++)
+	{
+		result += Digit % 10;
+		Digit = Digit / 10;
+	}
+
+	return int.Parse(result);
+}
