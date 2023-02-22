1f64aaa6c13876b3015fdb7c58a8a0bf0005b61c
Nursat <makhsotnursat@gmail.com>
2023-02-22 14:44:39 +0500

    Добавлена задача на поиск максимального значения в массиве

---
+++ FindMax/Program.cs
@@ -0,0 +1,27 @@
+﻿
+var n = int.Parse(Console.ReadLine());
+
+var array = new int[n];
+var rnd = new Random(Guid.NewGuid().GetHashCode());
+
+for (int i = 0;i < n; i++)
+{
+	array[i] = rnd.Next(0, 100);
+	Console.Write(array[i] + " ");
+}
+
+Console.WriteLine();
+
+var maxVal = array[0];
+var index = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+	if (maxVal < array[i])
+	{
+		maxVal = array[i];
+		index = i;
+	}
+}
+
+Console.WriteLine($"\nMax value is {maxVal} in array index of [{index + 1}]");
