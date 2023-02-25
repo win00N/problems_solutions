985e322187061b440cb4a90d717ba88ccdf8247a
Nursat <makhsotnursat@gmail.com>
2023-02-25 17:58:46 +0500

    Add RecursionTask

---
+++ RecursionTask/Program.cs
@@ -0,0 +1,21 @@
+﻿int PrintRec(int a, int b)
+{
+	Console.Write(a + " ");
+	if (a == b)
+	{
+		return a;
+	}
+
+	if (a < b)
+	{
+		return PrintRec(++a, b);
+	}
+	else
+	{
+		return PrintRec(--a, b);
+	}
+}
+
+PrintRec(1, 10);
+Console.WriteLine();
+PrintRec(10, 1);
